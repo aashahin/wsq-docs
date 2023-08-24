39a44dac2193d6b0ca041b9378de70e5090cc51c
Abdelrahman Shaheen <abdelrahmanshaheeen8@gmail.com>
2023-08-24 14:40:04 +0300

    Inital Docs

---
+++ main.js
@@ -0,0 +1,12 @@
+const route = window.location.hash;
+const indexPage = document.getElementById('index');
+const databasePage = document.getElementById('database');
+
+if (route === '#index') {
+    indexPage.style.display = 'block';
+    databasePage.style.display = 'none';
+}
+else if (route === '#database') {
+    indexPage.style.display = 'none';
+    databasePage.style.display = 'block';
+}
