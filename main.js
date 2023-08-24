b4c213a7af2e73c4f1b230a89ddf5aaa64126805
Abdelrahman Shaheen <abdelrahmanshaheeen8@gmail.com>
2023-08-24 15:12:39 +0300

    Inital Docs

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 const route = window.location.hash;
 const indexPage = document.getElementById('index');
+const featuresPage = document.getElementById('features');
+const requirementsPage = document.getElementById('requirements');
 const databasePage = document.getElementById('database');
 
 if (route === '#index') {
     indexPage.style.display = 'block';
     databasePage.style.display = 'none';
+    featuresPage.style.display = 'none';
+    requirementsPage.style.display = 'none';
+}
+else if (route === '#features') {
+    indexPage.style.display = 'none';
+    databasePage.style.display = 'none';
+    requirementsPage.style.display = 'none';
+}
+else if (route === '#requirements') {
+    indexPage.style.display = 'none';
+    databasePage.style.display = 'none';
+    featuresPage.style.display = 'none';
 }
 else if (route === '#database') {
     indexPage.style.display = 'none';
     databasePage.style.display = 'block';
+    featuresPage.style.display = 'none';
+    requirementsPage.style.display = 'none';
 } else {
     indexPage.style.display = 'block';
     databasePage.style.display = 'none';
+    featuresPage.style.display = 'none';
+    requirementsPage.style.display = 'none';
 }
