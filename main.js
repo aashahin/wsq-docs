24bfa32ad832ef96e4704e807b0b0a3821712886
Abdelrahman Shaheen <abdelrahmanshaheeen8@gmail.com>
2023-08-25 13:57:24 +0300

    Update Content

--- main.js
+++ main.js
@@ -1,33 +1,43 @@
 const route = window.location.hash;
-const indexPage = document.getElementById('index');
-const featuresPage = document.getElementById('features');
-const requirementsPage = document.getElementById('requirements');
-const databasePage = document.getElementById('database');
+const indexPage = document.getElementById("index");
+const featuresPage = document.getElementById("features");
+const requirementsPage = document.getElementById("requirements");
+const databasePage = document.getElementById("database");
+const installPage = document.getElementById("install");
 
-if (route === '#index') {
-    indexPage.style.display = 'block';
-    databasePage.style.display = 'none';
-    featuresPage.style.display = 'none';
-    requirementsPage.style.display = 'none';
-}
-else if (route === '#features') {
-    indexPage.style.display = 'none';
-    databasePage.style.display = 'none';
-    requirementsPage.style.display = 'none';
-}
-else if (route === '#requirements') {
-    indexPage.style.display = 'none';
-    databasePage.style.display = 'none';
-    featuresPage.style.display = 'none';
-}
-else if (route === '#database') {
-    indexPage.style.display = 'none';
-    databasePage.style.display = 'block';
-    featuresPage.style.display = 'none';
-    requirementsPage.style.display = 'none';
+if (route === "#index") {
+  databasePage.style.display = "none";
+  featuresPage.style.display = "none";
+  requirementsPage.style.display = "none";
+  installPage.style.display = "none";
+} else if (route === "#features") {
+  document.title = "المميزات - وسَق";
+  indexPage.style.display = "none";
+  databasePage.style.display = "none";
+  requirementsPage.style.display = "none";
+  installPage.style.display = "none";
+} else if (route === "#requirements") {
+  document.title = "المتطلبات - وسَق";
+  indexPage.style.display = "none";
+  databasePage.style.display = "none";
+  featuresPage.style.display = "none";
+  installPage.style.display = "none";
+} else if (route === "#database") {
+  document.title = "تثبيت قاعدة البيانات - وسَق";
+  indexPage.style.display = "none";
+  featuresPage.style.display = "none";
+  requirementsPage.style.display = "none";
+  installPage.style.display = "none";
+} else if (route === "#install") {
+  document.title = "تثبيت النظام - وسَق";
+  indexPage.style.display = "none";
+  databasePage.style.display = "none";
+  featuresPage.style.display = "none";
+  requirementsPage.style.display = "none";
 } else {
-    indexPage.style.display = 'block';
-    databasePage.style.display = 'none';
-    featuresPage.style.display = 'none';
-    requirementsPage.style.display = 'none';
+  document.title = "وسَق";
+  databasePage.style.display = "none";
+  featuresPage.style.display = "none";
+  requirementsPage.style.display = "none";
+  installPage.style.display = "none";
 }
