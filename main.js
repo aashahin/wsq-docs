6618a825b1b87bf7bc826757f1c3dede2b0236b2
Abdelrahman Shaheen <abdelrahmanshaheeen8@gmail.com>
2023-08-26 11:50:55 +0300

    Update Content

--- main.js
+++ main.js
@@ -4,36 +4,49 @@ const featuresPage = document.getElementById("features");
 const requirementsPage = document.getElementById("requirements");
 const databasePage = document.getElementById("database");
 const installPage = document.getElementById("install");
+const wsqPage = document.getElementById("wsq");
 
 if (route === "#index") {
   databasePage.style.display = "none";
   featuresPage.style.display = "none";
   requirementsPage.style.display = "none";
   installPage.style.display = "none";
+  wsqPage.style.display = "none";
 } else if (route === "#features") {
   document.title = "المميزات - وسَق";
   indexPage.style.display = "none";
   databasePage.style.display = "none";
   requirementsPage.style.display = "none";
   installPage.style.display = "none";
+  wsqPage.style.display = "none";
 } else if (route === "#requirements") {
   document.title = "المتطلبات - وسَق";
   indexPage.style.display = "none";
   databasePage.style.display = "none";
   featuresPage.style.display = "none";
   installPage.style.display = "none";
+  wsqPage.style.display = "none";
 } else if (route === "#database") {
   document.title = "تثبيت قاعدة البيانات - وسَق";
   indexPage.style.display = "none";
   featuresPage.style.display = "none";
   requirementsPage.style.display = "none";
   installPage.style.display = "none";
+  wsqPage.style.display = "none";
 } else if (route === "#install") {
   document.title = "تثبيت النظام - وسَق";
   indexPage.style.display = "none";
   databasePage.style.display = "none";
   featuresPage.style.display = "none";
   requirementsPage.style.display = "none";
+  wsqPage.style.display = "none";
+} else if (route === "#wsq") {
+  document.title = "شرح النظام";
+  indexPage.style.display = "none";
+  databasePage.style.display = "none";
+  featuresPage.style.display = "none";
+  requirementsPage.style.display = "none";
+  installPage.style.display = "none";
 } else {
   document.title = "وسَق";
   databasePage.style.display = "none";
